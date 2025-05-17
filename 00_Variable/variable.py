376f59a3ab6160dc62157f36d5af90866a1ca543
sopladoman <166187700+alexsilvauam@users.noreply.github.com>
2025-05-16 21:37:32 -0600

    feat 00_variables

---
+++ 00_Variable/variable.py
@@ -0,0 +1,27 @@
+#Variable = A container for storing data values. (String, Integer, Float, Boolean)
+#           A variable is created the moment you first assign a value to it.
+#           A variable name can only contain letters, numbers, or underscores (A-z, 0-9, or _)
+
+# String 
+first_name = "Bro"
+food = "Pizza"
+email = "Bro123@fake.com"
+
+# Integer
+age = 20
+quantity = 5
+num_of_cars = 2
+
+# Float
+height = 5.9
+weight = 150.5
+
+# Boolean
+is_student = True
+is_employed = False
+
+if is_student:
+    print("You are a student.")
+else:
+    print("You are not a student.")
+
